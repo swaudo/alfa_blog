523f4c85ec2d81f241342eb4b7508307674bb639
swaudo <simemaw30@gmail.com>
2012-12-27 23:34:17 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlfaBlog::Application.config.secret_token = '61b40b7fdd6b2e043a6213dfa7efce4e757f32947a2e298d7fd33ffb21e8edde673722d5aeebdac6a20d82967553805472b73343f28b9d3b06445d6f10e071ce'
